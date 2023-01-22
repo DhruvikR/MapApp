e3e11e2677de06f663732e80d46aa024fce72fab
DhruvikR <dhruvikrangani3@gmail.com>
2023-01-22 22:55:03 +0530

    Commit

--- app/src/main/java/com/example/mapapp/MapsActivity.java
+++ app/src/main/java/com/example/mapapp/MapsActivity.java
@@ -1,6 +1,7 @@
 package com.example.mapapp;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
@@ -18,9 +19,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +32,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private ActivityMapsBinding binding;
     List<Address> listGeoCoder;
     private static final int LOCATION_PERMISSION_CODE=101;
+    private String address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +62,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
             try{
-                listGeoCoder=new Geocoder(this).getFromLocationName("BAPS Swaminarayan Chhatralay (APC), Anand - Vidyanagar Road, Ketivadi, Vallabh Vidyanagar, Anand, Gujarat",1);
+                listGeoCoder=new Geocoder(this).getFromLocationName("BAPS Swaminarayan Chhatralay (APC), " +
+                        "Anand - Vidyanagar Road, Ketivadi, Vallabh Vidyanagar, Anand, Gujarat",1);
             }
             catch(Exception e)
             {
@@ -75,20 +81,42 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 //            toast.setMargin(50,50);
 //            toast.show();
 
-            LatLng latlagofAPC=new LatLng(latitude,longitude);
-            mMap.addMarker(new MarkerOptions().position(latlagofAPC).title("APC"));
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlagofAPC));
-
+//            LatLng latlagofAPC=new LatLng(latitude,longitude);
+//            mMap.addMarker(new MarkerOptions().position(latlagofAPC).title("APC"));
+//            mMap.moveCamera(CameraUpdateFactory.newLatLng(latlagofAPC));
+            Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
             mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
                 @Override
                 public void onMapClick(@NonNull LatLng latLng) {
-                    mMap.addMarker(new MarkerOptions().position(latLng).title("Click Here"));
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mMap.clear();
+                    try{
+                        List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                        Address obj = addresses.get(0);
+                        address = obj.getAddressLine(0);
+                        String textforToast=address+"\nPlease Click on Marker for Set Notification or Alarm";
+                        Toast toast=Toast.makeText(getApplicationContext(),textforToast,Toast.LENGTH_SHORT);
+                        toast.setMargin(50,50);
+                        toast.show();
+                        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
 
-                    String s = "Address h" + "as Longitude ::: " + String.valueOf(latLng.longitude) + " And Latitude ::: " + String.valueOf(latLng.latitude);
-                    Toast toast=Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT);
-                    toast.setMargin(50,50);
-                    toast.show();
+                    }
+                    catch(IOException e){
+                        System.out.println(e);
+                    }
+                }
+            });
+
+            mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker) {
+                    // on marker click we are getting the title of our marker
+                    // which is clicked and displaying it in a toast message.
+                    Intent intent = new Intent(MapsActivity.this, markerClickActivity.class);
+                    intent.putExtra("Address_massage", address);
+                    startActivity(intent);
+
+                    return false;
                 }
             });
 
@@ -125,7 +153,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void requestLocationPermissions(){
-        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_PERMISSION_CODE);
+        ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                LOCATION_PERMISSION_CODE);
     }
 
 }
---
+++ app/src/main/java/com/example/mapapp/setNotificationActivity.java
@@ -0,0 +1,14 @@
+package com.example.mapapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class setNotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_notification);
+    }
+}
