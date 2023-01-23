13709c131d85ee24f7fa14a51a97ab943725b5a2
DhruvikR <dhruvikrangani3@gmail.com>
2023-01-23 10:54:10 +0530

    Commit

--- app/src/main/java/com/example/mapapp/markerClickActivity.java
+++ app/src/main/java/com/example/mapapp/markerClickActivity.java
@@ -1,41 +1,38 @@
 package com.example.mapapp;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class markerClickActivity extends AppCompatActivity {
 
-    TextView textViewForAddress=findViewById(R.id.textOfAdd);
-    Button btnAlarm=findViewById(R.id.btnForAlarm);
-    Button btnNoti=findViewById(R.id.btnForNotification);
+    TextView textView1=findViewById(R.id.textOfAdd);
+//    Button btnAlarm=findViewById(R.id.btnForAlarm);
+//    Button btnNoti=findViewById(R.id.btnForNotification);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker_click);
 
-        Intent intent=getIntent();
+//        Intent intent=getIntent();
 
-        String textForAddress = "Address : "+intent.getStringExtra("Address_massage");
+        String textForAddress = "Address : ";
 
-        textViewForAddress.setText(textForAddress);
+        textView1.setText(textForAddress);
 
 
 
     }
 
-    public void addAlarm(View view) {
-        Intent intentOfSetAlarm=new Intent(markerClickActivity.this,setAlarmActivity.class);
-        startActivity(intentOfSetAlarm);
-    }
-
-    public void addNotification(View view) {
-        Intent intentOfSetNotification=new Intent(markerClickActivity.this,setNotificationActivity.class);
-        startActivity(intentOfSetNotification);
-    }
+//    public void addAlarm(View view) {
+////        Intent intentOfSetAlarm=new Intent(markerClickActivity.this,setAlarmActivity.class);
+////        startActivity(intentOfSetAlarm);
+//    }
+//
+//    public void addNotification(View view) {
+////        Intent intentOfSetNotification=new Intent(markerClickActivity.this,setNotificationActivity.class);
+////        startActivity(intentOfSetNotification);
+//    }
 }
